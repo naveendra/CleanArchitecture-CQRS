cf699d92f31db9771e03e92599ae5ebb61d5ba6e
naveendra wattegedara <neo.wattegedara@gmail.com>
2026-04-13 10:30:51 +0530

    Add project files.

---
+++ Application.Application/Interfaces/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,20 @@
+﻿using Application.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Application.Interfaces.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        Task CreateEmployeeAsync(CreateEmployeeDto createEmployeeDto);
+        Task<List<GetEmployeeDto>> GetAllEmployeesAsync(int page, int pageSize);
+
+        Task<GetEmployeeDto> GetEmployeeByIdAsync(int id);
+
+        Task DeleteEmployeeAsync(int id);
+
+        Task UpdateEmployeeAsync(int id, UpdateEmployeeDto updateEmployeeDto);
+
+    }
+}
